51d58ea8c5666fe2f8a16e2deb1403ca2ecb4127
yujo <yujo@student.42seoul.kr>
2020-06-17 16:29:09 +0900

    hosting test

--- youtube_clone/index.js
+++ youtube_clone/index.js
@@ -7,4 +7,17 @@ function handleListening() {
   console.log(`Listening on : http://localhost:${PORT}`);
 }
 
+function handleHome(req, res) {
+  console.log(req);
+  res.send("Hello frome Home");
+}
+
+function handleProfile(req, res) {
+  res.send("You are on my profile");
+}
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
 app.listen(4000, handleListening);
