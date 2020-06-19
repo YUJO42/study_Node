74fad54dc45547d09931789716622c5ba6f435af
yujo <yujo@student.42seoul.kr>
2020-06-19 21:28:18 +0900

    add Model View Controller Pattern

---
+++ youtube_clone/controllers/userController.js
@@ -0,0 +1,7 @@
+export const join = (req, res) => res.send("JOIN");
+export const login = (req, res) => res.send("LOGIN");
+export const logout = (req, res) => res.send("LOGOUT");
+export const users = (req, res) => res.send("Users");
+export const userDetail = (req, res) => res.send("User Detail");
+export const editProfile = (req, res) => res.send("Edit Profile");
+export const changePassword = (req, res) => res.send("Change Password");
