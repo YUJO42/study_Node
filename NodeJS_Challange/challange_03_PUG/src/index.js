359710a5787a19cffc5f058078e8015d75b3af4c
yujo <yujo@student.42seoul.kr>
2020-06-22 13:12:47 +0900

    setting node, express, babel

---
+++ NodeJS_Challange/challange_03_PUG/src/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+import path from "path";
+
+const app = express();
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+
+// Add your magic here!
+
+// Codesanbox does not need PORT :)
+// app.listen(() => console.log(`Listening!`));
