740cb7948f24bbe18ef3a443a6e58007b930a94f
yujo <yujo@student.42seoul.kr>
2020-06-22 16:08:50 +0900

    pug가 안 붙는다 이유 찾아서 해결할 것

---
+++ NodeJS_Challange/challange_03_PUG/controller.js
@@ -0,0 +1,4 @@
+export const home = (req, res) => res.send("HOME");
+export const login = (req, res) => res.send("LOGIN");
+export const photos = (req, res) => res.send("PHOTOS");
+export const profile = (req, res) => res.send("PROFILE");
--- NodeJS_Challange/challange_03_PUG/src/index.js
+++ NodeJS_Challange/challange_03_PUG/src/index.js
@@ -1,11 +1,25 @@
 import express from "express";
 import path from "path";
+import routes from "../routes";
+import globalRouter from "../globalRouter";
+import { localsMiddleware } from "../middleware";
+
+const PORT = 3000;
+
+const handleListening = () =>
+  console.log(`Listening on : http://localhost:${PORT}`);
 
 const app = express();
-app.set("view engine", "pug");
-app.set("views", path.join(__dirname, "views"));
 
+app.use(localsMiddleware);
+
+app.set("view engine", "pug");
+app.set("views", "./views/index,pug");
+
+app.use(routes.home, globalRouter);
 // Add your magic here!
 
 // Codesanbox does not need PORT :)
 // app.listen(() => console.log(`Listening!`));
+
+app.listen(PORT, handleListening);
