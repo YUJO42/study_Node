0b5b21c3f7525b31a9fa816268559e2c825c027b
yujo <yujo@student.42seoul.kr>
2020-06-29 19:17:18 +0900

    nodeJS challange 04 - OUTPUT done

--- NodeJS_Challange/challange_04_Output/src/index.js
+++ NodeJS_Challange/challange_04_Output/src/index.js
@@ -2,11 +2,18 @@ import express from "express";
 import path from "path";
 import "./db";
 import movieRouter from "./movieRouter";
+import { localsMiddleware } from "./middlewares";
+
+const PORT = 3300;
+
+const handleListening = () =>
+  console.log(`Listening on : http://localhost:${PORT}`);
 
 const app = express();
+
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
+app.use(localsMiddleware);
 app.use("/", movieRouter);
 
-// Codesanbox does not need PORT :)
-app.listen(() => console.log(`✅  Server Ready!`));
+app.listen(PORT, handleListening);
--- NodeJS_Challange/challange_04_Output/src/movieController.js
+++ NodeJS_Challange/challange_04_Output/src/movieController.js
@@ -5,6 +5,39 @@ import {
   getMovieByMinimumYear,
 } from "./db";
 
-export const home = (req, res) => {};
-export const movieDetail = (req, res) => {};
-export const filterMovie = (req, res) => {};
+export const home = (req, res) =>
+  res.render("movies", { movies: getMovies(), pageTitle: "Movies!" });
+
+export const movieDetail = (req, res) => {
+  const {
+    params: { id },
+  } = req;
+  const movie = getMovieById(id);
+  if (!movie) {
+    res.render("404", { pageTitle: "Movie not found" });
+  }
+  return res.render("detail", { movie, pageTitle: movie.title });
+};
+
+export const filterMovie = (req, res) => {
+  const {
+    query: { year, rating },
+  } = req;
+  if (year) {
+    const movies = getMovieByMinimumYear(year);
+    return res.render("movies", {
+      pageTitle: `Searching by year: ${year}`,
+      searchingBy: "year",
+      searchingTerm: year,
+      movies,
+    });
+  }
+  if (rating) {
+    const movies = getMovieByMinimumRating(rating);
+    return res.render("movies", {
+      pageTitle: `Searching by rating: ${rating}`,
+      movies,
+    });
+  }
+  res.render("404", { pageTitle: "Movie not found" });
+};
